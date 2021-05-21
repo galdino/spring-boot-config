4b32a77c84b9a6d7a534f2e2680e3ecd06c0a198
Fernando Galdino <fernando.galdino@meta.com.br>
2021-05-21 08:58:44 -0300

    Add spring cloud client dependencies

--- src/main/java/br/com/springbootconfig/GreetingController.java
+++ src/main/java/br/com/springbootconfig/GreetingController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,10 +24,18 @@ public class GreetingController {
 	@Autowired
 	private DbSettings dbSettings;
 	
+	@Autowired
+	private Environment environment;
+	
 	@GetMapping("/greeting")
 	public String greeting() {
 //		return this.greetingMessage + this.listValues + this.mapValues;
 		return this.dbSettings.getConnection() + this.dbSettings.getHost();
+	}
+	
+	@GetMapping("/envdetails")
+	public String envdetails() {
+		return this.environment.toString();
 	}
 
 }
